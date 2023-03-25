b8b1cc43b431b5f1e1af7eb6414a28290e86bff5
MariusAdrianStoica <stoichitza@yahoo.com>
2023-03-25 21:03:45 +0100

    project setup complete

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+const greetingMessage =<div>Hello React!</div>;
+console.log(">>>>>>> Hello React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(greetingMessage);
+
